daca4d3033dd80937cae8dc0bd0744ebcf71ce0f
Tobias Knöpfli <113889152+tobiasknoepfli@users.noreply.github.com>
2023-06-27 15:20:25 +0200

    created "Constructors"

---
+++ src/Constructors.java
@@ -0,0 +1,40 @@
+public class Constructors {
+    public static class Person {
+        private String name;
+        private int age;
+        private String origin;
+
+//        DEFAULT CONSTRUCTOR
+        public Person() {
+            name = "";
+        }
+
+//        PARAMETERISED CONSTRUCTOR
+        public Person(String name, int age){
+            this.name = name;
+            this.age = age;
+        }
+
+//        OVERLOADED CONSTRUCTOR
+        public Person(String name, int age, String origin){
+            this.name = name;
+            this.age = age;
+            this.origin = origin;
+        }
+
+        public void introduce(){
+            System.out.println("I'm " + name + ", I'm " + age + " years old and I am " + origin);
+        }
+    }
+
+    public static void main(String[] args) {
+        Person ida = new Person();
+        Person john = new Person("John",99);
+        Person herbert = new Person("Herbert",76,"German");
+
+        ida.introduce();
+        john.introduce();
+        herbert.introduce();
+
+    }
+}
